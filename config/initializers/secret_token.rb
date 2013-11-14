b5bba50c981c4a10eadb316f289bd89f4b317e65
Kohei Iwasaki <iwark02@gmail.com>
2013-11-14 14:54:02 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IwarkStudyMeeting::Application.config.secret_key_base = '371aba75a59edd3c119cff15f04ba71996ffda5bc2cd5d391bd5a805a1f18f6ce3824990d5a75c431c93e46bafd0d47f3b15d34b1a203c9d72426de283ddedc3'
